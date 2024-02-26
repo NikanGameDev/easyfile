cbfce3104cd5992221b04b194f22b2ccd2f8acf7
NikanGameDev <87191838+NikanGameDev@users.noreply.github.com>
2024-02-26 16:23:17 -0500

    Update module.py

--- module.py
+++ module.py
@@ -4,9 +4,9 @@ def write(path, text):
 def writebin(path, bytes):
 	with open(path, "wb") as a:
 		a.write(bytes)
-def read(path, text):
+def read(path):
 	with open(path, "rt") as a:
 		a.read()
-def readbin(path, bytes):
+def readbin(path):
 	with open(path, "rb") as a:
 		a.read()
