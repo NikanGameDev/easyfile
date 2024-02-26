fd25f743eedce39c527bae033e44a3d02ee6c25a
NikanGameDev <87191838+NikanGameDev@users.noreply.github.com>
2024-02-26 16:07:27 -0500

    Updated files

--- module.py
+++ module.py
@@ -1,12 +1,12 @@
-export def write(path, text):
+def write(path, text):
 	with open(path, "wt") as a:
 		a.write(text)
-export def writebin(path, bytes):
+def writebin(path, bytes):
 	with open(path, "wb") as a:
 		a.write(bytes)
-export def read(path, text):
+def read(path, text):
 	with open(path, "rt") as a:
 		a.read()
-export def read(path, bytes):
+def readbin(path, bytes):
 	with open(path, "rb") as a:
 		a.read()
